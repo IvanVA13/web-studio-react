454e9b13542a646f8a50fd228a909c3a2f4b9200
Ivan Volobuev <ivannnn2425@gmail.com>
2022-01-14 18:05:25 +0200

    add demo settings component

--- src/components/SettingsList/SettingsList.js
+++ src/components/SettingsList/SettingsList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SettingsList.module.scss';
 import { Formik, Form, Field, useField } from 'formik';
 import * as Yup from 'yup';
@@ -6,40 +6,26 @@ import * as Yup from 'yup';
 const MyTextInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <div className={styles['form-container']}>
+    <>
       <label className={styles['form-label']} htmlFor={props.id || props.name}>
-        {label}
+        <span>{label}</span>
         {meta.touched && meta.error ? (
           <div className={styles['form-error']}>*{meta.error}</div>
         ) : null}
       </label>
-      <input className={styles['form-input']} {...field} {...props} />
-    </div>
-  );
-};
-
-const MyDownloadInput = ({ label, ...props }) => {
-  // const [field, meta] = useField(props);
-  return (
-    <div className={styles['field__wrapper']}>
-      <input
-        className={styles['field__file']}
-        name="file"
-        type="file"
-        id="field__file-2"
-      />
-
-      <label className={styles['field__file-wrapper']} for="field__file-2">
-        <div className={styles['field__file-fake']}>Файл не вбран</div>
-        <div className={styles['field__file-button']}>Выбрать</div>
-      </label>
-    </div>
+      <div className={styles['form-container']}>
+        <input className={styles['form-input']} {...field} {...props} />
+        <button className={styles['form-btn']} type="submit">
+          Иземенить
+        </button>
+      </div>
+    </>
   );
 };
 
 const MyRadioBtn = ({ children, ...props }) => {
   return (
-    <label className={styles['form-label__radio']}>
+    <label className={styles['form-radio']}>
       <Field
         className={styles['form-radio-btn']}
         type="radio"
@@ -51,184 +37,244 @@ const MyRadioBtn = ({ children, ...props }) => {
   );
 };
 
-export const SettingsList = () => {
+const MyDownloadInput = ({ label, ...props }) => {
+  const [fakeField, setFakeField] = useState(null);
+
+  const handleChange = e => {
+    setFakeField(e?.target?.files[0]?.name);
+  };
+
   return (
-    <div className={`container ${styles['form-main-container']}`}>
-      <Formik
-        initialValues={{
-          firstName: '',
-        }}
-        validationSchema={Yup.object({
-          firstName: Yup.string().required('required'),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            resetForm();
-          }, 400);
-        }}
-      >
-        <Form className={styles.form}>
-          <MyTextInput
-            label="First Name"
-            name="firstName"
-            type="text"
-            placeholder="John"
-            autoComplete="off"
-          />
-          <button className={styles['form-btn']} type="submit">
-            Change
-          </button>
-        </Form>
-      </Formik>
-      <Formik
-        initialValues={{
-          lastName: '',
-        }}
-        validationSchema={Yup.object({
-          lastName: Yup.string().required('required'),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            resetForm();
-          }, 400);
-        }}
-      >
-        <Form className={styles.form}>
-          <MyTextInput
-            label="Last Name"
-            name="lastName"
-            type="text"
-            placeholder="Doe"
-            autoComplete="off"
-          />
-          <button className={styles['form-btn']} type="submit">
-            Change
-          </button>
-        </Form>
-      </Formik>
-      <Formik
-        initialValues={{
-          email: '',
-        }}
-        validationSchema={Yup.object({
-          email: Yup.string()
-            .email('Invalid email address')
-            .required('required'),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            resetForm();
-          }, 400);
-        }}
-      >
-        <Form className={styles.form}>
-          <MyTextInput
-            label="Email Address"
-            name="email"
-            type="email"
-            placeholder="user@webstudio.com"
-            autoComplete="off"
-          />
-          <button className={styles['form-btn']} type="submit">
-            Change
-          </button>
-        </Form>
-      </Formik>
-      <Formik
-        initialValues={{
-          password: '',
-        }}
-        validationSchema={Yup.object({
-          password: Yup.string().min(6).required('Required'),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            resetForm();
-          }, 400);
-        }}
-      >
-        <Form className={styles.form}>
-          <MyTextInput
-            label="Password"
-            name="password"
-            type="password"
-            placeholder="password"
-            autoComplete="off"
-          />
-          <button className={styles['form-btn']} type="submit">
-            Change
-          </button>
-        </Form>
-      </Formik>
-      <Formik
-        initialValues={{
-          sex: '',
-        }}
-        validationSchema={Yup.object({
-          sex: Yup.string().required('required'),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            resetForm();
-          }, 400);
-        }}
-      >
-        <Form className={styles.form}>
-          <div className={styles['form-container']}>
-            <div className={styles['form-label']} id="my-radio-group">
-              Sex
-            </div>
-            <div role="group" aria-labelledby="my-radio-group">
-              <MyRadioBtn name="sex">male</MyRadioBtn>
-              <MyRadioBtn name="sex">female</MyRadioBtn>
-            </div>
+    <div className={styles['input-download-wrapper']}>
+      <input
+        className={styles['input-download']}
+        name="file"
+        type="file"
+        id="field__file-2"
+        {...props}
+        onChange={handleChange}
+      />
+
+      <label className={styles['input-download-label']} htmlFor="field__file-2">
+        {label}
+        <div className={styles['form-container']}>
+          <div className={styles['input-download-fake']}>
+            {fakeField ? fakeField : 'Выбрать файл'}
           </div>
           <button className={styles['form-btn']} type="submit">
-            Change
+            Иземенить
           </button>
-        </Form>
-      </Formik>
-      <Formik
-        initialValues={{
-          avatar: '',
-        }}
-        validationSchema={Yup.object({
-          avatar: Yup.string().required('required'),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            resetForm();
-          }, 400);
-        }}
-      >
-        <Form
-          method="post"
-          enctype="multipart/form-data"
-          className={styles.form}
-        >
-          <MyDownloadInput
-            label="Avatar"
-            name="avatar"
-            type="file"
-            accept="image/*"
-          />
-          <button className={styles['form-btn']} type="submit">
-            Change
-          </button>
-        </Form>
-      </Formik>
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export const SettingsList = () => {
+  return (
+    <div className="container">
+      <ul className={`${styles['settings-list']}`}>
+        <li className={`${styles['settings-list-item']}`}>
+          <Formik
+            initialValues={{
+              firstName: '',
+            }}
+            validationSchema={Yup.object({
+              firstName: Yup.string().required('обязательно'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                resetForm();
+              }, 400);
+            }}
+          >
+            <Form className={styles.form}>
+              <MyTextInput
+                label="Изменить имя"
+                name="firstName"
+                id="firstName"
+                type="text"
+                placeholder="John"
+                autoComplete="off"
+              />
+            </Form>
+          </Formik>
+        </li>
+        <li className={`${styles['settings-list-item']}`}>
+          <Formik
+            initialValues={{
+              lastName: '',
+            }}
+            validationSchema={Yup.object({
+              lastName: Yup.string().required('обязательно'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                resetForm();
+              }, 400);
+            }}
+          >
+            <Form className={styles.form}>
+              <MyTextInput
+                label="Изменить фамилию"
+                name="lastName"
+                id="lastName"
+                type="text"
+                placeholder="Doe"
+                autoComplete="off"
+              />
+            </Form>
+          </Formik>
+        </li>
+        <li className={`${styles['settings-list-item']}`}>
+          <Formik
+            initialValues={{
+              email: '',
+            }}
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .email('Указан неверный email')
+                .required('обязательно'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                resetForm();
+              }, 400);
+            }}
+          >
+            <Form className={styles.form}>
+              <MyTextInput
+                label="Изменить Email"
+                name="email"
+                id="email"
+                type="email"
+                placeholder="user@webstudio.com"
+                autoComplete="off"
+              />
+            </Form>
+          </Formik>
+        </li>
+        <li className={`${styles['settings-list-item']}`}>
+          <Formik
+            initialValues={{
+              password: '',
+            }}
+            validationSchema={Yup.object({
+              password: Yup.string().min(6).required('обязательно'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                resetForm();
+              }, 400);
+            }}
+          >
+            <Form className={styles.form}>
+              <MyTextInput
+                label="Изменить пароль"
+                name="password"
+                id="password"
+                type="password"
+                placeholder="password"
+                autoComplete="off"
+              />
+            </Form>
+          </Formik>
+        </li>
+        <li className={`${styles['settings-list-item']}`}>
+          <Formik
+            initialValues={{
+              sex: '',
+            }}
+            validationSchema={Yup.object({
+              sex: Yup.string().required('обязательно'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                resetForm();
+              }, 400);
+            }}
+          >
+            <Form className={styles.form}>
+              <div className={styles['form-container']}>
+                <div className={styles['form-label']} id="my-radio-group">
+                  Изменить пол
+                </div>
+                <div role="group" aria-labelledby="my-radio-group">
+                  <MyRadioBtn name="sex">мужской</MyRadioBtn>
+                  <MyRadioBtn name="sex">женский</MyRadioBtn>
+                </div>
+                <button className={styles['form-btn']} type="submit">
+                  Иземенить
+                </button>
+              </div>
+            </Form>
+          </Formik>
+        </li>
+        <li className={`${styles['settings-list-item']}`}>
+          <Formik
+            initialValues={{
+              avatar: '',
+            }}
+            validationSchema={Yup.object().shape({
+              avatar: Yup.mixed().required('обязательно'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              console.log(values.avatar.name);
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                resetForm();
+              }, 400);
+            }}
+          >
+            {({ setFieldValue }) => (
+              <Form
+                // method="post"
+                encType="multipart/form-data"
+                className={styles.form}
+              >
+                <div className={styles['input-download-wrapper']}>
+                  <input
+                    className={styles['input-download']}
+                    name="avatar"
+                    accept="image/*"
+                    type="file"
+                    id="field__file-2"
+                    onChange={event => {
+                      setFieldValue('avatar', event.currentTarget.files[0]);
+                    }}
+                  />
+
+                  <label
+                    className={styles['input-download-label']}
+                    htmlFor="field__file-2"
+                  >
+                    Иземенить аватар
+                    <div className={styles['form-container']}>
+                      <div className={styles['input-download-fake']}>
+                        Выбрать файл
+                      </div>
+                    </div>
+                  </label>
+                </div>
+                <button className={styles['form-btn']} type="submit">
+                  Иземенить
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </li>
+      </ul>
     </div>
   );
 };
