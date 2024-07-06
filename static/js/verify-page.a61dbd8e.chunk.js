8fd3780666d636c7b8f302607d5c6df765eeff7b
IvanVA13 <71542875+IvanVA13@users.noreply.github.com>
2024-07-06 22:56:55 +0300

    Updates

---
+++ static/js/verify-page.a61dbd8e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpweb-studio-react"]=this["webpackJsonpweb-studio-react"]||[]).push([[15],{293:function(t,e,s){"use strict";s.r(e);var c=s(15),a=s(12),b=s(46),o=s(4),r=s(6),i=s(3);e.default=()=>{const t=Object(c.b)(),e=Object(a.g)();return e?(t(Object(o.o)(e.params.id)),Object(i.jsx)(a.a,{to:r.a.home})):Object(i.jsx)(b.a,{})}}}]);
+//# sourceMappingURL=verify-page.a61dbd8e.chunk.js.map
